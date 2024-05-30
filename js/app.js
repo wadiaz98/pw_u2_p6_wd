0b20cd30f6377ef27aab880d5a22eb9f68a7ace5
wadiaz@uce.edu.ec <wadiaz@uce.edu.ec>
2024-05-29 19:31:27 -0500

    taller13

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
 console.log(Vue);
 
+const estudiantes = [
+  { nombre: "Willan", Apellido: "Diaz" },
+  { nombre: "Alexander", Apellido: "Cordova" },
+  { nombre: "Moris", Apellido: "Goku" },
+];
+
 const app = Vue.createApp({
-    template: `
+  /* template: `
     <h1>Hola mundo desde Vue.JS</h1>
     <p>{{1+1}}</p>
     <p>{{[1,2,3,4,5]}}</p>
     <p>{{true? "Verdadero": "Falso"}}</p>
 
-    `
+    ` */
+  data() {
+    return {
+      mensaje: "Hola mundo Propiedad reactivas",
+      valor: 777,
+      lista: estudiantes,
+    };
+  },
+  methods: {
+    cambiarTexto() {
+      this.mensaje = "Texto cambiado";
+    },
+    sumar() {
+      this.valor += 1;
+    },
+    agregar(){
+        const nuevo = {nombre:"Perico", Apellido:"Palotes"};
+        this.lista.unshift(nuevo);
+    }
+  },
 });
 
 app.mount("#myApp");
